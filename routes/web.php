4ed8f646c288087be91f39b61a6ab93d402716b3
ADJHz <adolfodjhz@gmail.com>
2026-09-19 17:44:59 -0600

    Caratula y metadata

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// A stable, fresh URL for sharing the birthday surprise on messaging apps.
+Route::view('/para-michell', 'welcome');
